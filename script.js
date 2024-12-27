b61704e06f3f338ace4d55e75d3082451ea6270d
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2024-12-27 17:54:40 +0100

    first commit - design page d'accueil

---
+++ script.js
@@ -0,0 +1,18 @@
+// Sélectionne les éléments
+const hamburger = document.getElementById('hamburger');
+const navigation = document.querySelector('.navigation');
+
+// Ajoute un événement au clic sur le hamburger
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active'); // Transforme les barres en croix
+    navigation.classList.toggle('active'); // Affiche/cache le menu mobile
+});
+
+// Optionnel : Fermer le menu au clic sur un lien
+const links = document.querySelectorAll('.navigation a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navigation.classList.remove('active');
+    });
+});
