14205f995ea05ff3d83d7c460fd7a285c0ec496a
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2024-12-28 13:45:35 +0100

    modifs sections home

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ links.forEach(link => {
         navigation.classList.remove('active');
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
